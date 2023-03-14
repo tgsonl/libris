98b225615a3ba6c48568e6bd546997daa5212d0b
Toby Slater <toby@tgs.onl>
2023-03-14 19:21:11 +0000

    Install Axios and begin scafolding SearchScreen.

---
+++ screens/SearchScreen.js
@@ -0,0 +1,9 @@
+import react, {useState} from  "react";
+import {View, Text, StyleSheet, Flatlist} from "react-native";
+import SearchBar from "../components/SearchBar"
+import Booklist from "../components/Booklist"
+import axios from "axios"
+
+const SearchScreen = () => {
+    // useState for searchText and booklist
+}
